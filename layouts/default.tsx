d402b92beb06aaf2e8dbb32460dd30cca0a59a82
Nuttakarn P <hazentio@gmail.com>
2025-03-27 02:27:42 +0700

    test: 💍 update ui - optional trim out func

--- layouts/default.tsx
+++ layouts/default.tsx
@@ -1,13 +1,14 @@
+/* eslint-disable prettier/prettier */
 import {Head} from './head';
 
 import {Navbar} from '@/components/navbar';
 
 export default function DefaultLayout({children}: {children: React.ReactNode}) {
   return (
-    <div className='relative flex flex-col h-screen'>
+    <div className='relative flex flex-col h-screen justify-between'>
       <Head />
       <Navbar />
-      <main className='container mx-auto max-w-7xl px-6 flex-grow pt-16'>{children}</main>
+      <main className='container mx-auto max-w-7xl px-6 w-full flex items-center justify-center'>{children}</main>
       <footer className='w-full flex items-center justify-center py-3'>
         <span className='text-xs text-default-600 pr-2'>Copyright © 2025</span>
         <p className='text-xs text-cyan-700'>DevGotGun all rights reserved</p>
