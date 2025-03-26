0d4811cf353920ccf3c373e43b452fbbde4456c9
Nuttakarn P <hazentio@gmail.com>
2025-03-26 17:57:01 +0700

    feat: 🎸 func push original path external

--- components/InputPath.tsx
+++ components/InputPath.tsx
@@ -1,53 +1,99 @@
+/* eslint-disable prettier/prettier */
 import {Button} from '@heroui/button';
-import {Code} from '@heroui/code';
+import {Input} from '@heroui/input';
 import {Snippet} from '@heroui/snippet';
 import {useState} from 'react';
+import {RocketIcon, UnLinkIcon} from './icon';
 
 const UrlInput = () => {
   const [url, setUrl] = useState('');
-  const [trimmedUrl, setTrimmedUrl] = useState('');
+  const [showOption, setShowOption] = useState(false);
+  const [shortenUrl, setShortenUrl] = useState('');
+  const [trimmedUrl, setTrimmedUrl] = useState('result convert url');
+  const [regex, setRegex] = useState('?');
 
   const handleUrlChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     setUrl(e.target.value);
   };
 
-  const trimUrl = () => {
-    // Trim the URL before '?' and set the trimmed URL state
-    const resultUrl = url.split('?')[0];
+  const handleDefaultUrl = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setShortenUrl(e.target.value);
+  };
 
+  const addCustomRegex = () => {
+    setShowOption((prev) => !prev);
+  };
+
+  const trimUrl = () => {
+    const resultUrl = url.split(regex)[0];
     setTrimmedUrl(resultUrl);
   };
 
-  return (
-    <div className='mt-8'>
-      {/* Input for URL */}
-      <input
-        className='mt-4 w-full mr-4 p-2 border border-gray-300 rounded'
-        placeholder='Enter URL'
-        type='text'
-        value={url}
-        onChange={handleUrlChange}
-      />
+  const extractUrl = () => {
+    window.open(shortenUrl, '_blank');
+    const resultUrl = '';
 
-      {/* Button to trigger URL trimming */}
+    // setTrimmedUrl(resultUrl);
+  };
+
+  const updateRegex = (e: any) => {
+    setRegex(e.target.value);
+  };
+
+  return (
+    <div className='flex flex-col w-[90vw] xl:w-[60vw] gap-4'>
+      <div className='flex gap-3 items-center justify-between w-full'>
+        <Input
+          required
+          className='w-[90%]'
+          placeholder='shorten url ://vt.tiktok.com/..'
+          type='text'
+          value={shortenUrl}
+          onChange={handleDefaultUrl}
+        />
+        <Button
+          color='default'
+          onPress={extractUrl}>
+          <RocketIcon />
+        </Button>
+      </div>
+      <div className='flex gap-3 items-center justify-between w-full'>
+        <Input
+          required
+          placeholder='Enter URL'
+          type='text'
+          value={url}
+          onChange={handleUrlChange}
+        />
+        {!showOption ? (
+          <Button
+            style={{backgroundColor: '#eee'}}
+            onPress={addCustomRegex}>
+            <UnLinkIcon />
+          </Button>
+        ) : (
+          <Input
+            className='w-fit'
+            label='trim out after'
+            placeholder='regex'
+            type='text'
+            value={regex}
+            onChange={updateRegex}
+          />
+        )}
+      </div>
       <Button
-        className='mt-4'
         color='primary'
+        disabled={!url}
         onPress={trimUrl}>
         Trim URL
       </Button>
-
-      {/* Display the trimmed URL */}
-
-      <div className='mt-4'>
-        <p>Trimmed URL:</p>
-        <Snippet
-          fullWidth
-          hideSymbol
-          variant='bordered'>
-          {trimmedUrl && <Code color='default'>{trimmedUrl}</Code>}
-        </Snippet>
-      </div>
+      <Snippet
+        fullWidth
+        hideSymbol
+        color='secondary'>
+        {trimmedUrl}
+      </Snippet>
     </div>
   );
 };
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,23 +1,43 @@
-import {heroui} from "@heroui/theme"
+import {heroui} from '@heroui/theme';
 
 /** @type {import('tailwindcss').Config} */
 const config = {
-  content: [
-    './layouts/**/*.{js,ts,jsx,tsx,mdx}',
-    './pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './components/**/*.{js,ts,jsx,tsx,mdx}',
-    "./node_modules/@heroui/theme/dist/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ['./layouts/**/*.{js,ts,jsx,tsx,mdx}', './pages/**/*.{js,ts,jsx,tsx,mdx}', './components/**/*.{js,ts,jsx,tsx,mdx}', './node_modules/@heroui/theme/dist/**/*.{js,ts,jsx,tsx}'],
   theme: {
+    screens: {
+      xs: '375px',
+      sm: '425px',
+      md: '768px',
+      lg: '1024px',
+      xl: '1280px',
+      xxl: '2560px',
+    },
     extend: {
       fontFamily: {
-        sans: ["var(--font-sans)"],
-        mono: ["var(--font-mono)"],
+        sans: ['var(--font-sans)'],
+        mono: ['var(--font-mono)'],
+      },
+      colors: {
+        common: {
+          black: '#000',
+          white: '#fff',
+        },
+        initial: '#CCCCCC',
+        prime: '#465670',
+        primary: '#4c84b5',
+        default: '#8defdb',
+        secondary_1: '#007991',
+        secondary_2: '#4d9ee6',
+        // secondary: '#A14686',
+        warning_0: '#f9d29f',
+        warning: '#F6B969',
+        warning_1: '#f29a26',
+        error: '#F24C44',
       },
     },
   },
-  darkMode: "class",
+  darkMode: 'class',
   plugins: [heroui()],
-}
+};
 
 export default config;
